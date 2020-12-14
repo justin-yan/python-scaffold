ec47d3a53e7818f1e42c701b972e8765a4c678b1
Justin <justin@yan.family>
2020-12-13 22:23:34 -0800

    Adding a DSU

---
+++ scaffold/graph/dsu.py
@@ -0,0 +1,27 @@
+class DSU:
+    """
+    Implementing a disjoint-set union structure.
+    """
+
+    def __init__(self, n):
+        self.pointer_array = list(range(n))
+        self.sizes = [1] * n
+
+    def find(self, p):
+        while p != self.pointer_array[p]:
+            # Path compression on repetitive queries
+            p = self.pointer_array[p] = self.pointer_array[self.pointer_array[p]]
+        return p
+
+    def union(self, p, q):
+        p_root = self.find(p)
+        q_root = self.find(q)
+        if p_root == q_root:
+            return
+
+        if self.sizes[p_root] < self.sizes[q_root]:
+            self.pointer_array[p_root] = q_root
+            self.sizes[q_root] += self.sizes[p_root]
+        else:
+            self.pointer_array[q_root] = p_root
+            self.sizes[p_root] += self.sizes[q_root]
